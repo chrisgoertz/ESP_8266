f642ab866313f6a7db04d0b7fa1587cbc05ce7e9
chris goertz <chris.goertz@gmx.de>
2023-04-10 11:59:32 +0200

    edit header

--- esp_at.h
+++ esp_at.h
@@ -25,6 +25,12 @@
 #define ESP_AT_CONFIGURE_AP			"AT+CWSAP"
 #define ESP_AT_LIST_STATIONS		"AT+CWLIF"
 #define ESP_AT_CONNECTION_STATUS	"AT+CIPSTATUS"
+#define ESP_AT_ESTABLISH_CONNECTION	"AT+CIPSTART"
+#define ESP_AT_OBTAIN_IP			"AT+CIFSR"
+#define ESP_AT_MULTIPLE_TCP_CONNECTIONS	"AT+CIPMUX"
+#define ESP_AT_TCP_SERVER			"AT+CIPSERVER"
+#define ESP_AT_OTA_UPDATE				"AT+CIUPDATE"
+
 
 
 
@@ -50,12 +56,19 @@ void esp_restart();
 void esp_get_version();
 void esp_set_mode(esp_at_modes_t mode);
 void esp_list_access_points();
-void esp_connect(char* ssid, char* password);
-void esp_disconnect();
+void esp_connect_to_ssid(char* ssid, char* password, char* mac);
+
+void esp_disconnect_from_ssid();
 void esp_get_ip();
 void esp_set_ip(char* ip);
 void esp_configure_access_point();
 void esp_list_connected_stations();
 void esp_get_connection_status();
+void esp_establish_connection();
+void esp_obtain_ip();
+void esp_set_single_connection();
+void esp_set_multiple_connections();
+
+
 
 #endif /* INC_ESP_AT_H_ */
