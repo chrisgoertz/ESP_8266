f08ba38de740e128b550f888f1a1a40760af80db
chris goertz <chris.goertz@gmx.de>
2023-04-10 12:12:43 +0200

    added status typedefs

--- esp_at.h
+++ esp_at.h
@@ -15,25 +15,27 @@
 #ifndef INC_ESP_AT_H_
 #define INC_ESP_AT_H_
 
-#define ESP_AT_CHECK 				"AT"
-#define ESP_AT_RESTART 				"AT+RST"
-#define ESP_AT_VERSION 				"AT+GMR"
-#define ESP_AT_MODE 				"AT+CWMODE"
-#define ESP_AT_LIST_ACCESS_POINTS 	"AT+CWLAP"
-#define ESP_AT_CONNECT 				"AT+CWJAP"
-#define ESP_AT_IP_ADDRESS			"AT+CIPSTA"
-#define ESP_AT_CONFIGURE_AP			"AT+CWSAP"
-#define ESP_AT_LIST_STATIONS		"AT+CWLIF"
-#define ESP_AT_CONNECTION_STATUS	"AT+CIPSTATUS"
-#define ESP_AT_ESTABLISH_CONNECTION	"AT+CIPSTART"
-#define ESP_AT_OBTAIN_IP			"AT+CIFSR"
+
+// AT COMMAND SET
+#define ESP_AT_CHECK 					"AT"
+#define ESP_AT_RESTART 					"AT+RST"
+#define ESP_AT_VERSION 					"AT+GMR"
+#define ESP_AT_MODE 					"AT+CWMODE"
+#define ESP_AT_LIST_ACCESS_POINTS 		"AT+CWLAP"
+#define ESP_AT_CONNECT 					"AT+CWJAP"
+#define ESP_AT_IP_ADDRESS				"AT+CIPSTA"
+#define ESP_AT_CONFIGURE_AP				"AT+CWSAP"
+#define ESP_AT_LIST_STATIONS			"AT+CWLIF"
+#define ESP_AT_CONNECTION_STATUS		"AT+CIPSTATUS"
+#define ESP_AT_ESTABLISH_CONNECTION		"AT+CIPSTART"
+#define ESP_AT_OBTAIN_IP				"AT+CIFSR"
 #define ESP_AT_MULTIPLE_TCP_CONNECTIONS	"AT+CIPMUX"
-#define ESP_AT_TCP_SERVER			"AT+CIPSERVER"
+#define ESP_AT_TCP_SERVER				"AT+CIPSERVER"
 #define ESP_AT_OTA_UPDATE				"AT+CIUPDATE"
 
 
 
-
+//
 /* AT_MODE enum*/
 typedef enum{
 	illegal = 0,
@@ -51,22 +53,58 @@ typedef enum{
 	connection_failed = 4
 }ESP_ConnectionStateTypeDef;
 
-void esp_check_usart();
-void esp_restart();
-void esp_get_version();
-void esp_set_mode(esp_at_modes_t mode);
-void esp_list_access_points();
-void esp_connect_to_ssid(char* ssid, char* password, char* mac);
+typedef enum{
+	open = 0,
+	wpa_psk = 2,
+	wpa2_psk = 3,
+	wpa_wpa2_psk = 4
 
+}ESP_EncryptionMethodTypeDef;
+
+typedef enum{
+	not_hidden = 0,
+	hidden = 1
+}ESP_SSID_HiddenStatusTypeDef;
+
+typedef enum{
+	optained = 2,
+	created_transmission = 3,
+	disconnected = 4,
+	does_not_connect = 5
+}ESP_TCP_UDP_ConnectionStatusTypeDef;
+
+
+// test uart connection
+void esp_check_usart();
+// restart module
+void esp_restart();
+// get version information from mudule
+void esp_get_version();
+// set wifi mode
+void esp_set_mode(esp_at_modes_t mode);
+// receive list of available access points
+void esp_list_access_points();
+// connect to specific ssid
+void esp_connect_to_ssid(char* ssid, char* password, char* mac);
+// disconnect from ssid
 void esp_disconnect_from_ssid();
+// receive the ip of the wifi module
 void esp_get_ip();
+// set the ip of the wifi module
 void esp_set_ip(char* ip);
+// configure the wifi modules access point
 void esp_configure_access_point();
+// list clients connected to the wifi module
 void esp_list_connected_stations();
+// get the connection status from the module
 void esp_get_connection_status();
-void esp_establish_connection();
+// establish tcp connection to specific ip
+void esp_establish_tcp_connection();
+//
 void esp_obtain_ip();
+// set the connection mode to single connection
 void esp_set_single_connection();
+// set the connection mode to multiple connections
 void esp_set_multiple_connections();
 
 
